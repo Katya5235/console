1029cf14191206465d342fc5b59592c36d256296
Ekaterina <tuc5235@gmail.com>
2023-02-25 14:34:03 +0300

    разобралась как искать в массиве индекс элмента

--- example_find/Program.cs
+++ example_find/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Поиск элемента в массиве
 
-int [] array = {1,2,3,4,5,6,7,8,};      //определим массив
+int[] array = { 1, 25, 3, 4, 25, 67, 7, 84, };
+int n = array.Length;
+int find = 25;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
