84c142fca75698da5c26b91ce7878e13e4babb51
Ekaterina <tuc5235@gmail.com>
2023-02-25 13:47:53 +0300

    parsed arrays

--- example002_002/Program.cs
+++ example002_002/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Будем избаляться от большого кол-ва пременных - Массивы
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 14045, 52, 63, 544, 95, 96, 777, 28, 89 };
+
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+Console.WriteLine(max);
