bf3ced84b172c850ad683ed83c8040803654481e
Ekaterina <tuc5235@gmail.com>
2023-03-05 20:55:38 +0300

    Massive sorting

---
+++ lekcia3/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+int[] arr = { 5, 2, 6, 8, 9, 4, 8 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+
+        for (int k = i + 1; k < array.Length; k++)
+        {
+            if (array[k] < array [min]) min = k;
+        }
+
+
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
